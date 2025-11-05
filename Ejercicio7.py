ff008adc5d9b3aefded2db2782cdede322c701ac
Franco <fnarazaflores@danielcastelao.org>
2025-11-05 12:39:12 +0100

    Ejercicio Realizado

---
+++ Ejercicio7.py
@@ -0,0 +1,10 @@
+###### ejercicio 7 #####
+"""
+Transformar la cadena de texto " Phytoneros" a mayúsculas. Guardar el valor
+en la variable y posteriormente convertirla nuevamente a minúsculas.
+"""
+texto = " Phytoneros"
+mayusculas = texto.upper()
+print(mayusculas)
+minusculas = mayusculas.lower()
+print(minusculas)

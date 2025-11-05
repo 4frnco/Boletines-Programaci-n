b974560033632ea9cf3c285dfe92e6d16facd66c
Franco <fnarazaflores@danielcastelao.org>
2025-11-05 12:39:53 +0100

    Ejercicio Realizado

---
+++ Ejercicio9.py
@@ -0,0 +1,8 @@
+###### ejercicio 9 #####
+"""
+Sobre la cadena de texto " Jeve jeve jeve", sustituir todas las vocales e
+por la vocal a dando como resultado "java java java".
+"""
+texto = " Jeve jeve jeve"
+reemplazado = texto.replace("e", "a")
+print(reemplazado)

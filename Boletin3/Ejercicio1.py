0417aaa959bb1541a87b009f40a17b9c134a9a97
frcnko <fnarazaflores@danielcastelao.org>
2025-11-05 01:10:50 +0100

    Ejercicio realizado

---
+++ Boletin3/Ejercicio1.py
@@ -0,0 +1,10 @@
+#################### ejercicio 1 ##################
+"""
+Codificar un programa que solicite un número por teclado y
+que muestre un mensaje que diga "Es un número positivo",
+siempre que cumpla esa condición.
+"""
+condicion = float(input("Dime un número: "))
+
+if condicion >= 0:
+    print("Es un número positivo")

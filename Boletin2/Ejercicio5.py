3877b13cf9a1f3eb6d5f063240aea8d6f4a3f88e
frcnko <fnarazaflores@danielcastelao.org>
2025-11-05 01:06:25 +0100

    Ejercicio realizado

---
+++ Boletin2/Ejercicio5.py
@@ -0,0 +1,10 @@
+################# ejercicio 5 ##################
+"""
+Escribir un programa que lea el valor de una distancia en millas marinas y la pase a
+metros (1 milla marina = 1852 m).
+"""
+millasmarinas = float(input("Dime un valor de la distancia en millas marinas:"))
+
+calculo = millasmarinas * 1852
+
+print("El calculo de la distancia en metros es:", calculo)

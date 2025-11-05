c2ca3a95304888b255090ed1f37e95635855f9c4
frcnko <fnarazaflores@danielcastelao.org>
2025-11-05 01:04:58 +0100

    Ejercicio realizado

---
+++ Boletin2/Ejercicio2.py
@@ -0,0 +1,7 @@
+################ ejercicio 2 ##################
+"""
+Realizar un ordinograma que permita calcular el área de un cuadrado de 3 m de lado.
+"""
+lado = 3
+area = lado * lado
+print("El área del cuadrado de 3 metros de lado es:", area, "m²")

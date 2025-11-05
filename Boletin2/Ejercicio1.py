b8458f8951274418a5fbf55e9e30b3f64189f880
frcnko <fnarazaflores@danielcastelao.org>
2025-11-05 01:03:40 +0100

    Ejercicio realizado

---
+++ Boletin2/Ejercicio1.py
@@ -0,0 +1,11 @@
+########## ejercicio 1 ###############
+"""
+Diseñar un algoritmo que calcule el área de un triángulo. La salida será por pantalla.
+Para codificar este programa inicializa la base al valor 4, y la altura al valor 3.
+"""
+base = 4
+altura = 3
+
+area = (base * altura / 2)
+
+print("El area de un triangulo es:", area)

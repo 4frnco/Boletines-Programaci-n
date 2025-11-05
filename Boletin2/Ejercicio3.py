c33bbb554152019d768fd282573d72377a973527
frcnko <fnarazaflores@danielcastelao.org>
2025-11-05 01:05:29 +0100

    Ejercicio realizado

---
+++ Boletin2/Ejercicio3.py
@@ -0,0 +1,9 @@
+##################### ejercicio 3 ################
+"""
+Crear un algoritmo que cambie euros a dólares (El cambio se pide por teclado).
+"""
+cambio = float(input("Dime el cambio de 1 euro a ? dolar:"))
+cantidadeuros = float(input("Dime la cantidad de euros que vas a cambiar:"))
+resultado = (cambio * cantidadeuros)
+
+print("Este es el cambio total de tus euros a dolares:", resultado)

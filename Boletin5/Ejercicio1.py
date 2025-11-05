d180f8d0d8b8d22c84e370f221d66abd811b0d5d
frcnko <fnarazaflores@danielcastelao.org>
2025-11-05 01:18:04 +0100

    Ejercicio realizado

---
+++ Boletin5/Ejercicio1.py
@@ -0,0 +1,9 @@
+###### ejercicio 1 #####
+"""
+Escribir un ciclo definido para imprimir por pantalla todos los números entre 10 y 20.
+"""
+condicion = 11
+
+while condicion < 20:
+    print(condicion)
+    condicion += 1

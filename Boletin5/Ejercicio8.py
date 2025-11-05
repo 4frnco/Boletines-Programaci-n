1543a0f534297980d9650bfa2331c6097ff3984c
frcnko <fnarazaflores@danielcastelao.org>
2025-11-05 01:20:29 +0100

    Ejercicio realizado

---
+++ Boletin5/Ejercicio8.py
@@ -0,0 +1,10 @@
+###### ejercicio 8 #####
+"""
+Modificar el programa anterior para que pueda generar fichas de un juego que puede tener
+números de 0 a n.
+"""
+n = int(input("Introduce el número máximo del dominó: "))
+
+for i in range(n + 1):
+    for j in range(i, n + 1):
+        print(i, "|", j)
